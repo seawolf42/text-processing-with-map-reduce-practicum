5382418b22841cb2fc72b469c076aa13c0fb5e65
jeffrey k eliasen <jeff@jke.net>
2013-09-04 12:23:29 +0800

    sample corpus downloaded from http://www.ietf.org/rfc/rfc2795.txt

---
+++ src/main/java/ch02/WordCount1.java
@@ -0,0 +1,58 @@
+package org.jolirouge.ditp.ch02;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+// from figure 2.3, basic (primitive) word count
+
+public class WordCount1 {
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "Primitive Word Counts");
+
+		job.setJarByClass(WordCount1.class);
+
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
+
+class WordCountMapper extends Mapper<IntWritable, Text, Text, IntWritable> {
+	static final IntWritable one = new IntWritable(1);
+	
+	@Override
+	public void map(IntWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		for (String token : value.toString().split(" ")) {
+			context.write(new Text(token), one);
+		}
+	}
+}
+
+class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public void reduce(Text key, Iterable<IntWritable> values, Context context)
+			throws IOException, InterruptedException {
+		int count = 0;
+		for (IntWritable value : values) {
+			count += value.get();
+		}
+		context.write(key, new IntWritable(count));
+	}
+}

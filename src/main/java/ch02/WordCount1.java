83b3ccd44882a717a4bf4c0a47801fcafffffa48
jeffrey k eliasen <jeff@jke.net>
2013-09-04 13:49:07 +0800

    removed unused import

--- src/main/java/ch02/WordCount1.java
+++ src/main/java/ch02/WordCount1.java
@@ -6,7 +6,6 @@ import misc.WholeFileInputFormat;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;

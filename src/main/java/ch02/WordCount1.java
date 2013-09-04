4917422d0ddbb17e7a71601b5616ba3e6c54b5ab
jeffrey k eliasen <jeff@jke.net>
2013-09-04 23:41:59 +0800

    tokenized input

--- src/main/java/ch02/WordCount1.java
+++ src/main/java/ch02/WordCount1.java
@@ -1,6 +1,10 @@
 package ch02;
 
+//from figure 2.3, basic (primitive) word count
+
 import java.io.IOException;
+import java.text.BreakIterator;
+import java.util.Locale;
 
 import misc.WholeFileInputFormat;
 
@@ -14,8 +18,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
-// from figure 2.3, basic (primitive) word count
 
 public class WordCount1 {
 	public static void main(String[] args) throws Exception {
@@ -41,12 +43,42 @@ public class WordCount1 {
 
 class WordCountMapper extends Mapper<NullWritable, Text, Text, IntWritable> {
 	static final IntWritable one = new IntWritable(1);
-	
+
 	@Override
 	public void map(NullWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
-		for (String token : value.toString().split(" ")) {
-			context.write(new Text(token), one);
+
+		Locale locale = new Locale("en", "US");
+		BreakIterator sentenceIterator = BreakIterator.getSentenceInstance(locale);
+		BreakIterator wordIterator = BreakIterator.getWordInstance(locale);
+
+		String document = value.toString();
+
+		sentenceIterator.setText(value.toString());
+		int sentenceIndex = sentenceIterator.first();
+		int lastSentenceIndex;
+		while (BreakIterator.DONE != sentenceIndex) {
+			lastSentenceIndex = sentenceIndex;
+			sentenceIndex = sentenceIterator.next();
+			if ((BreakIterator.DONE != sentenceIndex) &&
+					Character.isLetterOrDigit(document.charAt(lastSentenceIndex))) {
+				String sentence = document.substring(lastSentenceIndex, sentenceIndex);
+				System.out.println("sentence: " + sentence);
+				wordIterator.setText(sentence);
+				int wordIndex;
+				int lastWordIndex;
+				wordIndex = wordIterator.first();
+				while (BreakIterator.DONE != wordIndex) {
+					lastWordIndex = wordIndex;
+					wordIndex = wordIterator.next();
+					if ((BreakIterator.DONE != wordIndex) &&
+							Character.isLetterOrDigit(sentence.charAt(lastWordIndex))) {
+						String token = sentence.substring(lastWordIndex, wordIndex);
+						System.out.println("token: " + token);
+						context.write(new Text(token), one);
+					}
+				}
+			}
 		}
 	}
 }

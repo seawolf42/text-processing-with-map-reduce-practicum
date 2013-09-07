ad759783fdb8b72179078c4d20f94a9cedd5b108
jeffrey k eliasen <jeff@jke.net>
2013-09-08 05:26:22 +0800

    primitive average computation

---
+++ src/main/java/ch03/MeanMapper.java
@@ -0,0 +1,17 @@
+package ch03;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class MeanMapper extends Mapper<Object, Text, Text, DoubleWritable> {
+	Text x = new Text("x");
+	
+	@Override
+	public void map(Object key, Text value, Context context)
+			throws IOException, InterruptedException {
+		context.write(x, new DoubleWritable(Double.parseDouble(value.toString())));
+	}
+}
---
+++ src/main/java/ch03/MeanReducer.java
@@ -0,0 +1,21 @@
+package ch03;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MeanReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+	@Override
+	public void reduce(Text key, Iterable<DoubleWritable> values, Context context)
+			throws IOException, InterruptedException {
+		double sum = 0.0;
+		int count = 0;
+		for (DoubleWritable value : values) {
+			sum += value.get();
+			++count;
+		}
+		context.write(key, new DoubleWritable(sum/count));
+	}
+}

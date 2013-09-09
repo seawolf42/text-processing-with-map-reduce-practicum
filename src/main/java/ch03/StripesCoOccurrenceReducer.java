e98cbd155273575b3d80d1617e64ca205dc1687d
jeffrey k eliasen <jeff@jke.net>
2013-09-09 13:56:05 +0800

    output frequencies instead of raw counts

--- src/main/java/ch03/StripesCoOccurrenceReducer.java
+++ src/main/java/ch03/StripesCoOccurrenceReducer.java
@@ -7,16 +7,19 @@ import misc.StringIntMapWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class StripesCoOccurrenceReducer extends Reducer<Text, StringIntMapWritable, Text, StringIntMapWritable> {
+public class StripesCoOccurrenceReducer extends Reducer<Text, StringIntMapWritable, Text, String> {
 	@Override
 	public void reduce(Text key, Iterable<StringIntMapWritable> values, Context context)
 			throws IOException, InterruptedException {
 		StringIntMapWritable counts = new StringIntMapWritable();
+		int marginal = 0;
 		for (StringIntMapWritable value : values) {
 			for (String innerKey : value.getCountKeys()) {
-				counts.addCount(innerKey, value.getCount(innerKey));
+				int count = value.getCount(innerKey);
+				marginal += count;
+				counts.addCount(innerKey, count);
 			}
 		}
-		context.write(key, counts);
+		context.write(key, counts.toFrequencyString(marginal));
 	}
 }
